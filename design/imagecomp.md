6ed4972133919ef76063a443749c299135c6fb14
Jemy <jemygraw@gmail.com>
2015-09-25 15:59:11 +0800

    update imagecomp preparation code.

--- design/imagecomp.md
+++ design/imagecomp.md
@@ -32,3 +32,9 @@ order取值
 
 0 表示从行开始依次粘贴(默认)
 1 表示从列开始依次粘贴
+
+提供的row和col的值必须能够和urls的数量匹配起来，最好是 row * col = len(urls)
+如果提供的 len(urls) > row * col 直接报错，url count larger than row * col;
+如果提供的 len(urls) < row * col ，则按照如下情况处理：
+1. 如果 order ＝ 0，即按照row的方式排列，那么如果 len(urls) < (row -1)* col 即报错
+2. 如果 order ＝ 1，即按照col的方式排列，那么如果 len(urls) < row * (col - 1) 即报错
--- src/ufop/imagecomp/imagecomp.go
+++ src/ufop/imagecomp/imagecomp.go
@@ -3,24 +3,46 @@ package imagecomp
 import (
 	"encoding/hex"
 	"errors"
+	"github.com/qiniu/api.v6/auth/digest"
+	"github.com/qiniu/rs"
+	"image"
 	"image/color"
+	"image/draw"
+	"image/jpeg"
+	"image/png"
 	"net/url"
+	"os"
+	"path/filepath"
 	"regexp"
 	"strconv"
+	"time"
 	"ufop"
 	"ufop/utils"
 )
 
 const (
-	H_ALIGN_LEFT   = "Left"
-	H_ALIGN_RIGHT  = "Right"
-	H_ALIGN_CENTER = "Center"
-	V_ALIGN_TOP    = "Top"
-	V_ALIGN_BOTTOM = "Bottom"
-	V_ALIGN_MIDDLE = "Middle"
+	IMAGECOMP_MAX_URL_COUNT = 1000
+
+	IMAGECOMP_ORDER_BY_ROW = 0
+	IMAGECOMP_ORDER_BY_COL = 1
+)
+
+const (
+	H_ALIGN_LEFT   = "left"
+	H_ALIGN_RIGHT  = "right"
+	H_ALIGN_CENTER = "center"
+	V_ALIGN_TOP    = "top"
+	V_ALIGN_BOTTOM = "bottom"
+	V_ALIGN_MIDDLE = "middle"
 )
 
 type ImageComposer struct {
+	mac *digest.Mac
+}
+
+type ImageComposerConfig struct {
+	AccessKey string `json:"access_key"`
+	SecretKey string `json:"secret_key"`
 }
 
 func (this *ImageComposer) Name() string {
@@ -28,6 +50,22 @@ func (this *ImageComposer) Name() string {
 }
 
 func (this *ImageComposer) InitConfig(jobConf string) (err error) {
+	confFp, openErr := os.Open(jobConf)
+	if openErr != nil {
+		err = errors.New(fmt.Sprintf("Open imagecomp config failed, %s", openErr.Error()))
+		return
+	}
+
+	config := ImageComposerConfig{}
+
+	decoder := json.NewDecoder(confFp)
+	decodeErr := decoder.Decode(&config)
+	if decodeErr != nil {
+		err = errors.New(fmt.Sprintf("Parse mkzip config failed, %s", decodeErr.Error()))
+		return
+	}
+
+	this.mac = &digest.Mac{config.AccessKey, []byte(config.SecretKey)}
 	return
 }
 
@@ -36,10 +74,10 @@ func (this *ImageComposer) InitConfig(jobConf string) (err error) {
 imagecomp
 /bucket/<string>
 /format/<string> 	optional, default jpg
-/halign/<string> 	optional, default Center
-/valign/<string> 	optional, default Middle
-/row/<int>			optional, default 1
-/col/<int>			optional, default 1
+/halign/<string> 	optional, default center
+/valign/<string> 	optional, default middle
+/rows/<int>			optional, default 1
+/cols/<int>			optional, default 1
 /order/<int>		optional, default 1
 /alpha/<int> 		optional, default 0
 /bgcolor/<string>	optional, default gray
@@ -48,8 +86,8 @@ imagecomp
 
 */
 func (this *ImageComposer) parse(cmd string) (bucket, format, halign, valign string,
-	row, col, order int, alpha bool, bgColor *color.Color, urls []map[string]string, err error) {
-	pattern = `^imagecomp/bucket/[0-9a-zA-Z-_=]+(/format/(png|jpg|jpeg)|/halign/(left|right|center)|/valign/(top|bottom|middle)|/row/\d+|/col/\d+|/order/(0|1)|/alpha/\d+|/bgcolor/[0-9a-zA-Z-_=]+){0,8}(/url/[0-9a-zA-Z-_=]+)+$`
+	rows, cols, order int, bgColor *color.Color, urls []map[string]string, err error) {
+	pattern = `^imagecomp/bucket/[0-9a-zA-Z-_=]+(/format/(png|jpg|jpeg)|/halign/(left|right|center)|/valign/(top|bottom|middle)|/rows/\d+|/cols/\d+|/order/(0|1)|/alpha/\d+|/bgcolor/[0-9a-zA-Z-_=]+){0,8}(/url/[0-9a-zA-Z-_=]+)+$`
 
 	matched, _ := regexp.Match(pattern, []byte(cmd))
 	if !matched {
@@ -86,13 +124,13 @@ func (this *ImageComposer) parse(cmd string) (bucket, format, halign, valign str
 
 	//check later by url count
 	//row
-	if rowStr := utils.GetParam(cmd, `row/\d+`, "row"); rowStr != "" {
-		row, _ = strconv.Atoi(rowStr)
+	if rowsStr := utils.GetParam(cmd, `row/\d+`, "row"); rowStr != "" {
+		rows, _ = strconv.Atoi(rowsStr)
 	}
 
 	//col
-	if colStr := utils.GetParam(cmd, `col/\d+`, "col"); colStr != "" {
-		col, _ = strconv.Atoi(colStr)
+	if colsStr := utils.GetParam(cmd, `col/\d+`, "col"); colStr != "" {
+		cols, _ = strconv.Atoi(colsStr)
 	}
 
 	//order
@@ -102,7 +140,7 @@ func (this *ImageComposer) parse(cmd string) (bucket, format, halign, valign str
 	}
 
 	//alpha
-	alpha = 0
+	alpha := 0
 	if alphaStr := utils.GetParam(cmd, "alpha/(0|1)", "alpha"); alphaStr != "" {
 		alpha, _ = strconv.Atoi(alphaStr)
 	}
@@ -156,9 +194,194 @@ func (this *ImageComposer) parse(cmd string) (bucket, format, halign, valign str
 		})
 	}
 
+	//check row and col valid or not
+	urlCount := len(urls)
+
+	if urlCount > IMAGECOMP_MAX_URL_COUNT {
+		err = errors.New(fmt.Sprintf("only allow url count not larger than %d", IMAGECOMP_MAX_URL_COUNT))
+		return
+	}
+
+	if urlCount > rows*cols {
+		err = errors.New("url count larger than rows*cols error")
+		return
+	}
+
+	if urlCount < rows*cols {
+		switch order {
+		case 0:
+			if urlCount < (rows-1)*cols {
+				err = errors.New("url count less than (rows-1)*cols error")
+				return
+			}
+		case 1:
+			if urlCount < rows*(cols-1) {
+				err = errors.New("url count less than rows*(cols-1) error")
+				return
+			}
+		}
+	}
+
 	return
 }
 
 func (this *ImageComposer) Do(req UfopRequest) (result interface{}, contentType string, err error) {
+	bucket, format, halign, valign, rows, cols, order, bgColor, urls, pErr := this.parse(req.Cmd)
+	if pErr != nil {
+		err = pErr
+		return
+	}
+
+	formatMimes := map[string]string{
+		"png":  "image/png",
+		"jpg":  "image/jpeg",
+		"jpeg": "image/jpeg",
+	}
+
+	//check urls validity, all should in bucket
+	statItems := make([]rs.EntryPath, 0)
+	for _, urlItem := range urls {
+		iPath := urlItem["path"]
+		entryPath := rs.EntryPath{
+			bucket, iPath,
+		}
+		statItems = append(statItems, entryPath)
+	}
+
+	qclient := rs.New(this.mac)
+
+	statRet, statErr := qclient.BatchStat(nil, statItems)
+	if statErr != nil {
+		err = errors.New(fmt.Sprintf("batch stat error, %s", statErr))
+		return
+	}
+
+	//download images by url
+	localImgPaths := make(map[string]string)
+	remoteImgUrls := make(map[string]string)
+	for _, urlItem := range urls {
+		iUrl := urlItem["url"]
+		iLocalName := fmt.Sprintf("imagecomp_tmp_%s_%d", utils.Md5Hex(iUrl), time.Now().UnixNano())
+		iLocalPath := filepath.Join(os.TempDir(), iLocalName)
+		contentType, dErr := utils.Download(iUrl, iLocalPath)
+		if dErr != nil {
+			err = dErr
+			return
+		}
+
+		if !(contentType == "image/png" || contentType == "image/jpeg") {
+			err = errors.New(fmt.Sprintf("unsupported mimetype of '%s', '%s'", iUrl, contentType))
+			return
+		}
+
+		localImgPaths[iLocalPath] = contentType
+		remoteImgUrls[iLocalPath] = iUrl
+	}
+
+	defer func() {
+		for iPath, _ := range localImgPaths {
+			os.Remove(iPath)
+		}
+	}()
+
+	//layout the images
+	localImgFps := make([]*os.File, 0)
+	var localImgObjs [rows][cols]*image.Image
+	var rowIndex int = 0
+	var colIndex int = 0
+
+	for iPath, iContentType := range localImgPaths {
+		imgFp, openErr := os.Open(iPath)
+		if openErr != nil {
+			err = errors.New(fmt.Sprintf("open local image of remote '%s' failed, %s", remoteImgUrls[iPath], openErr.Error()))
+			return
+		}
+		localImgFps = append(localImgFps, imgFp)
+
+		var imgObj *image.Image
+		var dErr error
+
+		if iContentType == "image/png" {
+			imgObj, dErr = png.Decode(imgFp)
+			if dErr != nil {
+				err = errors.New(fmt.Sprintf("decode png image of remote '%s' failed, %s", remoteImgUrls[iPath], dErr.Error()))
+				return
+			}
+		} else if iContentType == "image/jpeg" {
+			imgObj, dErr = jpeg.Decode(imgFp)
+			if dErr != nil {
+				err = errors.New(fmt.Sprintf("decode jpeg image of remote '%s' failed, %s", remoteImgUrls[iPath], dErr.Error()))
+				return
+			}
+		}
+
+		switch order {
+		case IMAGECOMP_ORDER_BY_ROW:
+			localImgObjs[rowIndex][colIndex] = imgObj
+
+			if colIndex < cols {
+				colIndex += 1
+			} else {
+				colIndex = 0
+				rowIndex += 1
+			}
+
+		case IMAGECOMP_ORDER_BY_COL:
+			localImgObjs[rowIndex][colIndex] = imgObj
+
+			if rowIndex < rows {
+				rowIndex += 1
+			} else {
+				rowIndex = 0
+				colIndex += 1
+			}
+		}
+	}
+
+	//close file handlers
+	defer func() {
+		for _, fp := range localImgFps {
+			fp.Close()
+		}
+	}()
+
+	//calc the dst image size
+
+	dstImageWidth := 0
+	dstImageHeight := 0
+
+	rowImageMaxWidths := make([]int, 0)
+	colImageMaxHeights := make([]int, 0)
+	for _, rowSlice := range localImgObjs {
+		rowImageMaxWidth := 0
+		colImageMaxHeight := 0
+		for _, imgObj := range rowSlice {
+			bounds := imgObj.Bounds()
+			rowImageMaxWidth += bounds.Max.X - bounds.Min.X
+			colImageMaxHeight += bounds.Max.Y - bounds.Min.Y
+		}
+
+		rowImageMaxWidths = append(rowImageMaxWidths, rowImageMaxWidth)
+		colImageMaxHeights = append(colImageMaxHeights, colImageMaxHeight)
+	}
+
+	dstImageWidth = utils.MaxInt(rowImageMaxWidths...)
+	dstImageHeight = utils.MaxInt(colImageMaxHeights...)
+
+	//compose the dst image
+	dstRect := image.Rect(0, 0, dstImageWidth, dstImageHeight)
+	dstImage := image.NewRGBA(dstRect)
+
+	//draw background
+	draw.Draw(dstImage, dstImage.Bounds(), bgColor, image.ZP, draw.Src)
+
+	drawStartPoint := image.ZP
+
+	for _, rowSlice := range localImgObjs {
+		for _, imgObj := range rowSlice {
+			//calc the draw start point
+
+		}
+	}
 	return
 }
